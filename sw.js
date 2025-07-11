3d936ccaf13136104b24835d972c4b98d70796bd
rsfigueroa32 <segurafigueroarodrigo@gmail.com>
2025-07-10 21:56:42 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+})
